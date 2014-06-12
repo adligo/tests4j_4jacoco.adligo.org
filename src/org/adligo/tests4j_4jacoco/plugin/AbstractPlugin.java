9b9be815d2fa5cfc0c33c2ccde2e4ecb9613295b
scott <scott@adligo.com>
2014-06-12 01:24:48 -0500

    good enough for tonight, remote run is working but early shutdown isn't

--- src/org/adligo/tests4j_4jacoco/plugin/AbstractPlugin.java
+++ src/org/adligo/tests4j_4jacoco/plugin/AbstractPlugin.java
@@ -85,6 +85,7 @@ public abstract class AbstractPlugin implements I_CoveragePlugin {
 		toRet.add("org.adligo.tests4j.models.shared.system.I_AssertListener");
 		toRet.add("org.adligo.tests4j.models.shared.system.I_CoveragePlugin");
 		toRet.add("org.adligo.tests4j.models.shared.system.I_Tests4J_Params");
+		toRet.add("org.adligo.tests4j.models.shared.system.I_Tests4J_RemoteInfo");
 		
 		toRet.add("org.adligo.tests4j_4jacoco.plugin.data.common.I_Probes");
 		
